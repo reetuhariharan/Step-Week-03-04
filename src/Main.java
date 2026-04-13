5d69908b2d3b67b7c54528cba506ea702b432991
reetuhariharan <reetuhariharan5@gmail.com>
2026-04-13 19:32:28 +0530

    UC2: Client Risk Ranking

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package problem1;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.*;
+
+class Transaction {
+    String id;
+    double fee;
+    String ts;
+
+    Transaction(String id, double fee, String ts) {
+        this.id = id;
+        this.fee = fee;
+        this.ts = ts;
+    }
+
+    public String toString() {
+        return id + ":" + fee + "@" + ts;
+    }
+}
+
+public class Main {
+
+    public static void bubbleSort(List<Transaction> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Collections.swap(list, j, j + 1);
+                }
+            }
+        }
+        System.out.println("Bubble: " + list);
+    }
+
+    public static void insertionSort(List<Transaction> list) {
+        for (int i = 1; i < list.size(); i++) {
+            Transaction key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && list.get(j).fee > key.fee) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, key);
+        }
+        System.out.println("Insertion: " + list);
+    }
+
+    public static void main(String[] args) {
+        List<Transaction> list = new ArrayList<>();
+        list.add(new Transaction("id1", 10.5, "10:00"));
+        list.add(new Transaction("id2", 25.0, "09:30"));
+        list.add(new Transaction("id3", 5.0, "10:15"));
+
+        bubbleSort(list);
+        insertionSort(list);
     }
 }
