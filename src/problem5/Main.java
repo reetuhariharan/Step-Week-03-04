66dea013ca76b3c4f6dee58ae8a3c29ec66f7c6b
reetuhariharan <reetuhariharan5@gmail.com>
2026-04-13 19:47:26 +0530

    UC4: Portfolio Return Sorting

---
+++ src/problem5/Main.java
@@ -0,0 +1,14 @@
+package problem4;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] returns = {12, 8, 15};
+
+        Arrays.sort(returns);
+
+        System.out.println(Arrays.toString(returns));
+    }
+}
