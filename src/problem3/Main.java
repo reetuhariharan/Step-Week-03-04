be3ce2c4a01590e231727b48ea343550a93c5c0f
reetuhariharan <reetuhariharan5@gmail.com>
2026-04-13 19:41:15 +0530

    UC3: Historical Trade Volume Analysis

---
+++ src/problem3/Main.java
@@ -0,0 +1,18 @@
+package problem3;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void mergeSort(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {500, 100, 300};
+
+        mergeSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
