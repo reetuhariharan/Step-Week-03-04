5de87a48ff36e2328ba2e1b5fd334069a779afd3
H. Reetu <reetuhariharan5@gmail.com>
2026-04-13 19:53:00 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package problem6;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        int[] risks = {10, 25, 50, 100};
+        int target = 30;
+
+        int floor = -1, ceil = -1;
+
+        for (int i = 0; i < risks.length; i++) {
+            if (risks[i] <= target) floor = risks[i];
+            if (risks[i] >= target) {
+                ceil = risks[i];
+                break;
+            }
+        }
+
+        System.out.println("Floor: " + floor);
+        System.out.println("Ceiling: " + ceil);
     }
 }
